c081af10a08bbc539ed8c294231445ffa5f86a20
Vishnu KS <yo@vishnuks.com>
2019-11-25 15:03:21 +0800

    Add courses and learn_python.

---
+++ learn_python/__init__.py
@@ -0,0 +1,14 @@
+index = {
+    1: {
+        "title": "Introduction",
+        "prefix": "introduction",
+    },
+    2: {
+        "title": "Variables",
+        "prefix": "variables",
+    },
+    3: {
+        "title": "For Loops",
+        "prefix": "for_loops",
+    },
+}
---
+++ learn_python/chapters/variables.md
@@ -0,0 +1,3 @@
+# Variables
+
+Hello variables
